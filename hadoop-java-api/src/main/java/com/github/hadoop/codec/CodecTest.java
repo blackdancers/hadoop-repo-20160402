c732f3e8553e2096064ae9c30ea93ab2e788be08
mushan <blackdancers@163.com>
2017-09-17 11:04:26 +0800

    codec

---
+++ hadoop-java-api/src/main/java/com/github/hadoop/codec/CodecTest.java
@@ -0,0 +1,15 @@
+package com.github.hadoop.codec;
+
+/**
+ * 
+ * @author ecmgr
+ *
+ */
+public class CodecTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
