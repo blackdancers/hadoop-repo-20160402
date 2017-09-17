de9f39bc2174520cb159eb531b1dcb1e9951d5a0
mushan <blackdancers@163.com>
2017-09-17 11:06:13 +0800

    update

--- hadoop-java-api/src/main/java/com/github/hadoop/codec/CodecTest.java
+++ hadoop-java-api/src/main/java/com/github/hadoop/codec/CodecTest.java
@@ -1,5 +1,19 @@
 package com.github.hadoop.codec;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionInputStream;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.DeflateCodec;
+import org.apache.hadoop.util.ReflectionUtils;
+import org.junit.Test;
+
 /**
  * 
  * @author ecmgr
@@ -7,9 +21,56 @@ package com.github.hadoop.codec;
  */
 public class CodecTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void compress() throws Exception {
+		String codecClassName = "org.apache.hadoop.io.compress.DefaultCodec";
+		Class<?> clazz = Class.forName(codecClassName);
+		Configuration conf = new Configuration();
+		CompressionCodec codec = (CompressionCodec)ReflectionUtils.newInstance(clazz, conf);
+		
+		FileInputStream fis = new FileInputStream("/data/soft/VM.tar.gz");
+		FileOutputStream fos = new FileOutputStream("/home/ecmgr/VM.deflate");
+		
+		CompressionOutputStream cos = codec.createOutputStream(fos);
+		IOUtils.copyBytes(fis, cos, 1024);
+		cos.finish();
+		cos.close();
+		fos.close();
+		fis.close();
+		System.out.println("ok");
 	}
+	
+	@Test
+	public void deCompress() throws FileNotFoundException, IOException {
+		Configuration conf = new Configuration();
+		Class<?> codecClazz = DeflateCodec.class;
+		DeflateCodec codec = (DeflateCodec) ReflectionUtils.newInstance(codecClazz, conf);
+		CompressionInputStream cis = codec.createInputStream(new FileInputStream("/home/ecmgr/VM.deflate"));
+		FileOutputStream fos = new FileOutputStream("/home/ecmgr/new_VM.tar.gz");
+		IOUtils.copyBytes(cis, fos, conf);
+		cis.close();
+		fos.close();
+		System.out.println("ok");
+	} 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
